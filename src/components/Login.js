dddedadfe72173de5abdde19752d4d8a43fd2d1d
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2024-07-09 20:54:13 -0500

    update-firebase

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import{useState}from 'react';
+import {auth} from "../firebaseConfig"
 
 const Login = () => {
+    const [userInfo , setUserInfo]  = useState({email: '', password:""});
+
+  const handleChange = (event) => {
+    setUserInfo({...userInfo,[event.target.name]:event.target.value});
+  }
+
+
   return (
     <div className="row mt-5">
       <div className="col"></div>
@@ -8,13 +16,17 @@ const Login = () => {
         <form className='form-group'>
             <input 
             type = "email"
+            name = {userInfo.name}
              className="form-control"
              placeholder="Type Your Email"
+             onChange={handleChange }
              />
             <input 
              type = "password"
+             name = {userInfo.name}
              className="form-control mt-4"
              placeholder="Type Your Password"
+             onChange={handleChange }
              />
               <input 
              type = "submit"
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,15 +1,15 @@
-import{initializeApp} from "firebase";
-import "firebase/auth"
+import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAUshQwGz_USz9TA1JVfv83OjzvAymO5TA",
-    authDomain: "restaurant-756f9.firebaseapp.com",
-    databaseURL: "https://restaurant-756f9-default-rtdb.firebaseio.com",
-    projectId: "restaurant-756f9",
-    storageBucket: "restaurant-756f9.appspot.com",
-    messagingSenderId: "722390473971",
-    appId: "1:722390473971:web:fbcef52964eb3c70f22b86"
-}
+  apiKey: "AIzaSyAUshQwGz_USz9TA1JVfv83OjzvAymO5TA",
+  authDomain: "restaurant-756f9.firebaseapp.com",
+  databaseURL: "https://restaurant-756f9-default-rtdb.firebaseio.com",
+  projectId: "restaurant-756f9",
+  storageBucket: "restaurant-756f9.appspot.com",
+  messagingSenderId: "722390473971",
+  appId: "1:722390473971:web:fbcef52964eb3c70f22b86"
+};
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = app.auth();
-export{auth}
+export{app}
